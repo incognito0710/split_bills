6329d7b154ea7072f9f83efa78de9165f2df1e34
Nikita yadav <148959709+incognito0710@users.noreply.github.com>
2025-09-15 12:11:22 +0530

    Delete src directory

--- src/Userbalance.java
+++
@@ -1,9 +0,0 @@
-public class Userbalance {
-    user user;
-    double amount;
-
-    public Userbalance(user user, double amount) {
-        this.user = user;
-        this.amount = amount;
-    }
-}
